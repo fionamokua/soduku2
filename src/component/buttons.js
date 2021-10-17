461a7fac3b1da8ea4a4963da85af6880e2a3eb4d
FionaM <mokuafiona@gmail.com>
2021-10-17 14:31:55 +0300

    new component with a new file

---
+++ src/component/buttons.js
@@ -0,0 +1,24 @@
+import React from "react";
+const Buttons =()=>
+{
+    
+    var buttonz=[];
+    var buttonum=9;
+    for (var i=0;i<buttonum;i++)
+    {
+        buttonz.push(<button className="small-buttons"></button>)
+
+    }
+//random selected represents the random number of buttons selected from 0 through 5
+//i shall call buttons that i have choosen to contain data as "selected" the nine buttons within a square
+// and "data " is  the numbers one through nine
+let data=[1,2,3,4,5,6,7,8,9]
+let selected=[i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7],i[8].i[9]]
+var randomselected=[1,2,3,4,5]
+
+    render()
+    {
+        return buttonz
+    }
+};
+export default Buttons
--- src/component/squares.js
+++ src/component/squares.js
@@ -1,33 +1,20 @@
 import { render } from "@testing-library/react";
 import React from "react";
 import "./square.css"
+import Buttons from "'/home/fiona/Desktop/solve/soduku/src/component/buttons.js"
 const Squares=()=>
 {
 
 var squarez=[];
 var squarenum=9;
-for (var i=0;i<squarenum;i++)
+for (let i=0;i<squarenum;i++)
 {
     squarez.push(<div className="squares"><Buttons/></div>)
+
 }
 render()
 {
     return squarez;
 }
-};
-const Buttons =()=>
-{
-    var buttonz=[];
-    var buttonum=9;
-    for (var i=0;i<buttonum;i++)
-    {
-        buttonz.push(<button className="small-buttons"></button>)
-    }
-    render()
-    {
-        return buttonz
-    }
-
-
 };
 export default Squares 
